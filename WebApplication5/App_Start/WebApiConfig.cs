3f93f3bbd37f1f11918d0075634cd3b1f7b6d1ff
Ezekiel Gallardo <ezekieldamian@gmail.com>
2017-04-14 16:10:30 -0400

    Added basic API

---
+++ WebApplication5/App_Start/WebApiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+
+namespace WebApplication5
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration configuration)
+        {
+            configuration.MapHttpAttributeRoutes();
+
+            configuration.Routes.MapHttpRoute(
+                name: "DefaultAPI",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+        }
+    }
+}
