c3ee47b6a8576ae39e0db2dac4e5f8f1cb79c481
Ezekiel Gallardo <ezekieldamian@gmail.com>
2017-04-14 16:39:56 -0400

    make JSON default return format for API

--- WebApplication5/App_Start/WebApiConfig.cs
+++ WebApplication5/App_Start/WebApiConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
 
 namespace WebApplication5
 {
@@ -12,6 +14,15 @@ namespace WebApplication5
                 name: "DefaultAPI",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional });
+
+            ////always return JSON!!!
+            //configuration.Formatters.JsonFormatter
+            //    .SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            var appXmlType = configuration.Formatters.XmlFormatter
+                .SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+
+            configuration.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
     }
 }
